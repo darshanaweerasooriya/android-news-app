ff78843df034dcba13a90ffa89fb79700bf1e9c1
darshana weerasooriya <96476541+darshanaweerasooriya@users.noreply.github.com>
2025-12-07 16:25:02 +0530

    Splash Screen done

---
+++ app/src/main/java/presentation/spalsh/splashActivity.kt
@@ -0,0 +1,21 @@
+package presentation.spalsh
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.android_news_app.R
+
+class splashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash2)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
